717d51c5180a74c5e6b8e3ea6e7669dde05e7b75
Fabio Previatti <fpreviatti@hotmail.com>
2022-04-10 13:24:33 -0300

    incluidas as rotas e instalado o pacote npm install react-router-dom@5

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-
     <div>
-      <HeaderComponent/>
+      <Router>
+        <div className="container">
+          <HeaderComponent />
 
-      <div className="container">
-        <ListEmployeeComponent/>
-      </div>
-      <FooterComponent/>
+          <div className="container">
+            <Switch>
+              <Route path="/" component = {ListEmployeeComponent}></Route>
+              <Route path="/employees" component = {ListEmployeeComponent}></Route>
+              <ListEmployeeComponent />
+            </Switch>
+          </div>
+          <FooterComponent />
+        </div>
+      </Router>
     </div>
-
-    
   );
 }
 
