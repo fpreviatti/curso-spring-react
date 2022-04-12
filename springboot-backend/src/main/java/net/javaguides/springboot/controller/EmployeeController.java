21b5aad0628719f04dea927f4d38d55a13606360
Fabio Previatti <fpreviatti@hotmail.com>
2022-04-12 13:35:25 -0300

    criada a funcionalidade de update para a API

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -34,4 +34,18 @@ public class EmployeeController {
 
        return ResponseEntity.ok(employee);
     }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " +id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(employee);
+        return ResponseEntity.ok(employee);
+    }
+
 }
